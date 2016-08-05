8e334b3f602dd0339bd26119e6d9b3d2cd87ade9
Will Strinz <wstrinz@gmail.com>
2016-08-05 09:28:37 -0500

    working elm loading

--- app/app.ts
+++ app/app.ts
@@ -18,9 +18,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      window.setTimeout(()=>{
-        window['loadElm'].call();
-      }, 3500)
     });
   }
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   constructor(private navCtrl: NavController) {
-  
+
+  }
+
+  ngAfterViewInit() {
+    window['loadElm'].call();
   }
 }
